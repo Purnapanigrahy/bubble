dd3e8fdacab99050130a6b8c31820e34ed7b15d8
Purnapanigrahy <163725020+Purnapanigrahy@users.noreply.github.com>
2025-11-19 20:33:49 +0530

    add the files into the repository.

---
+++ index.js
@@ -0,0 +1,37 @@
+function bubble(){
+    let bubble = "";
+    for (let i = 1;i <= 140;i++ ){
+        bubble += `<a href=""><div class="bubble">${Math.floor(Math.random()*10)}</div></a>`;
+    }
+    document.querySelector("#bottom").innerHTML = bubble;
+}
+function hit(){
+    let no = Math.floor(Math.random()*10)
+    document.querySelector(".scorebox").innerHTML = no;
+}  
+function trimer(){
+    let trimer = 60;
+    let seteinerval = setInterval(function(){
+        if (trimer > 0){
+            trimer--;
+        }
+        else{
+            clearInterval(seteinerval)
+        }
+        document.querySelectorAll(".scorebox")[1].innerHTML = trimer;
+    },1000)
+}  
+var score = 0
+function score_increse(){
+    score += 10
+    document.querySelectorAll(".scorebox")[2].innerHTML = score;
+}
+
+
+function call(){
+    bubble()
+    hit()
+    trimer()
+    score_increse()
+}
+call()
